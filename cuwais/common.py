148940988fbca00c8c2b8c155616da7a1c72da2f
Joe O'Connor <joeoc2625@gmail.com>
2020-12-19 17:34:56 +0000

    Fixed some issues with the outcome enum

--- cuwais/common.py
+++ cuwais/common.py
@@ -1,11 +1,12 @@
 import json
 from datetime import datetime
-from enum import Enum
+from enum import Enum, unique
 
 
+@unique
 class Outcome(Enum):
-    Win = 1,
-    Loss = 2,
+    Win = 1
+    Loss = 2
     Draw = 3
 
 
