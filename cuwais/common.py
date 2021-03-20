ebb1c9396c3c36f017934f90e6690901a8967b4a
Joe O'Connor <joeoc2625@gmail.com>
2021-03-20 00:51:32 +0000

    Added calculate hash function

--- cuwais/common.py
+++ cuwais/common.py
@@ -1,3 +1,4 @@
+import hashlib
 from enum import Enum, unique
 
 
@@ -6,3 +7,11 @@ class Outcome(Enum):
     Win = 1
     Loss = 2
     Draw = 3
+
+
+def calculate_git_hash(user_id: int, commit_hash: str, url: str) -> str:
+    m = hashlib.sha256()
+    m.update(str(user_id).encode('utf-8'))
+    m.update(commit_hash.encode('utf-8'))
+    m.update(url.encode('utf-8'))
+    return str(m.hexdigest())
