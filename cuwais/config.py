350fa5d4addb0ff0234691c5e731bfd6eb3aa50a
Joe O'Connor <joeoc2625@gmail.com>
2021-05-20 00:40:22 +0100

    Added a default config file option

--- cuwais/config.py
+++ cuwais/config.py
@@ -1,12 +1,57 @@
+import logging
+from typing import Optional, Union
+
 import yaml
+import os
+
+
+def _fill_in_defaults(data: Optional[Union[dict, list]], defaults: Optional[Union[dict, list]]):
+    if data is None:
+        return defaults
+
+    if isinstance(data, list):
+        return data
+
+    if isinstance(data, dict):
+        if not isinstance(defaults, dict):
+            return data
+
+        for key in data:
+            data[key] = _fill_in_defaults(data[key], defaults.get(key, None))
+
+        for key in defaults:
+            if key not in data:
+                data[key] = defaults[key]
+
+        return data
+
+    raise RuntimeError("Unknown data type: " + type(data))
 
 
 class Config:
-    def __init__(self, path):
+    def __init__(self, path: str, defaults: Optional[Union[dict, list, 'Config']] = None):
+        if isinstance(defaults, Config):
+            defaults = defaults.get_all()
+
+        self._defaults = defaults
+
+        if not os.path.isfile(path):
+            logging.error(f"Could not find config file {path}")
+            self._data = None
         with open(path, 'r') as stream:
             self._data = yaml.safe_load(stream)
 
-    def get(self, key: str, default=None):
+        if self._defaults is None:
+            self._defaults = dict()
+        if self._data is None:
+            self._data = dict()
+
+        self._data = _fill_in_defaults(self._data, defaults)
+
+    def get_all(self):
+        return self._data
+
+    def get(self, key: str):
         if not isinstance(key, str):
             raise RuntimeError("Key should be of type str")
 
@@ -14,26 +59,16 @@ class Config:
 
         data = self._data
         for part in parts:
+            if data is None:
+                break
             if isinstance(data, dict):
                 data = data.get(part, None)
             elif isinstance(data, list):
-                try:
-                    i = int(part)
-                except ValueError:
-                    return default
+                i = int(part)
                 data = None if i >= len(data) or i < 0 else data[i]
-            if data is None:
-                return default
-
-        types = [str, int, float]
-        for t in types:
-            if isinstance(default, t):
-                return t(data)
-
-        if isinstance(default, bool):
-            return str(data)[0].lower() in {"y", "t"}
 
         return data
 
 
-config_file = Config("config.yml")
+default_config_file = Config("default_config.yml")
+config_file = Config("config.yml", default_config_file)
